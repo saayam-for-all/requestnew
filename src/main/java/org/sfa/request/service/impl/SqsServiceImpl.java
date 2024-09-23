da24a37e5691435d673c2f53fcc92836fdbae95c
shrithika <68739370+shrithika22@users.noreply.github.com>
2024-09-23 11:50:57 -0400

    Added SqsService

---
+++ src/main/java/org/sfa/request/service/impl/SqsServiceImpl.java
@@ -0,0 +1,43 @@
+package org.sfa.request.service.impl;
+
+import org.sfa.request.service.api.SqsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.*;
+
+import java.util.Map;
+
+@Service
+public class SqsServiceImpl implements SqsService {
+
+
+    private final SqsClient sqsClient;
+
+    @Autowired
+    public SqsServiceImpl(SqsClient sqsClient) {
+        this.sqsClient = sqsClient;
+    }
+
+
+    public void sendMessage(String queueUrl, String phone, String email) {
+        SendMessageRequest sendMessageRequest =
+                SendMessageRequest.builder()
+                        .queueUrl(queueUrl)
+                        .messageBody("This is a SQS message")
+                        .messageAttributes(Map.of(
+                                "Phone", MessageAttributeValue.builder()
+                                        .dataType("String")
+                                        .stringValue(phone)
+                                        .build(),
+                                "Email", MessageAttributeValue.builder()
+                                        .dataType("String")
+                                        .stringValue(email)
+                                        .build()
+                        ))
+                        .build();
+
+        SendMessageResponse sendMessageResponse = sqsClient.sendMessage(sendMessageRequest);
+        System.out.println("Message Sent: " + sendMessageResponse.messageId());
+    }
+}
